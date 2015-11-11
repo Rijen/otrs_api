ef3964af0c1e5d304af9c82b5a7426da70bb5317
rijen <rijen@rijen-pc>
2015-11-11 19:39:40 +0600

    xxx

--- User/UserList.pm
+++ User/UserList.pm
@@ -1,26 +1,42 @@
-#!/usr/bin/perl
-#
-# @File UserGet.pm
-# @Author rijen
-# @Created 11.11.2015 18:27:15
-#
+# --
+# Kernel/GenericInterface/Operation/Test/Test.pm - GenericInterface test operation backend
+# Copyright (C) 2001-2015 OTRS AG, http://otrs.com/
+# --
+# This software comes with ABSOLUTELY NO WARRANTY. For details, see
+# the enclosed file COPYING for license information (AGPL). If you
+# did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
+# --
 
-package Kernel::GenericInterface::Operation::User::UserList;
+package Kernel::GenericInterface::Operation::Test::Test;
 
 use strict;
 use warnings;
 
-
-use Kernel::System::VariableCheck qw(IsArrayRefWithData IsHashRefWithData IsStringWithData);
-
-use base qw(
-    Kernel::GenericInterface::Operation::Common
-);
+use Kernel::System::VariableCheck qw(IsHashRefWithData);
 
 use Kernel::System::ObjectManager;
 local $Kernel::OM = Kernel::System::ObjectManager->new();
-
+	
 our $ObjectManagerDisabled = 1;
+
+=head1 NAME
+
+Kernel::GenericInterface::Operation::Test - GenericInterface Operation Test backend
+
+=head1 SYNOPSIS
+
+=head1 PUBLIC INTERFACE
+
+=over 4
+
+=cut
+
+=item new()
+
+usually, you want to create an instance of this
+by using Kernel::GenericInterface::Operation->new();
+
+=cut
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -29,11 +45,11 @@ sub new {
     bless( $Self, $Type );
 
     # check needed objects
-    for my $Needed (qw(DebuggerObject WebserviceID)) {
+    for my $Needed (qw(DebuggerObject)) {
         if ( !$Param{$Needed} ) {
             return {
                 Success      => 0,
-                ErrorMessage => "Got no $Needed!",
+                ErrorMessage => "Got no $Needed!"
             };
         }
 
@@ -43,25 +59,75 @@ sub new {
     return $Self;
 }
 
+=item Run()
+
+perform the selected test Operation. This will return the data that
+was handed to the function or return a variable data if 'TestError' and
+'ErrorData' params are sent.
+
+    my $Result = $OperationObject->Run(
+        Data => {                               # data payload before Operation
+            ...
+        },
+    );
+
+    $Result = {
+        Success         => 1,                   # 0 or 1
+        ErrorMessage    => '',                  # in case of error
+        Data            => {                    # result data payload after Operation
+            ...
+        },
+    };
+
+    my $Result = $OperationObject->Run(
+        Data => {                               # data payload before Operation
+            TestError   => 1,
+            ErrorData   => {
+                ...
+            },
+        },
+    );
+
+    $Result = {
+        Success         => 0,                                   # it always return 0
+        ErrorMessage    => 'Error message for error code: 1',   # including the 'TestError' param
+        Data            => {
+            ErrorData   => {                                    # same data was sent as
+                                                                # 'ErrorData' param
+
+            },
+            ...
+        },
+    };
+
+=cut
 
 sub Run {
     my ( $Self, %Param ) = @_;
 
-    my ( $UserID, $UserType ) = $Self->Auth(
-        %Param,
+	my $UserObject = $Kernel::OM->Get('Kernel::System::User');
+	my %List = $UserObject->UserList(
+        Type          => 'Short'
     );
 
-    return $Self->ReturnError(
-        ErrorCode    => 'UserGet.AuthFail',
-        ErrorMessage => "UserGet: Authorization failing!",
-    ) if !$UserID;
 
-
-    my $UserObject = $Kernel::OM->Get('Kernel::System::User');
-	my %List = $UserObject->UserList();
     # return result
-    return %List;
+    return {
+        Success => 1,
+        Data    => \%List,
+    };
 }
 
+1;
 
+=back
 
+=head1 TERMS AND CONDITIONS
+
+This software is part of the OTRS project (L<http://otrs.org/>).
+
+This software comes with ABSOLUTELY NO WARRANTY. For details, see
+the enclosed file COPYING for license information (AGPL). If you
+did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
+
+=cut
