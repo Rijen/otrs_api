87ddd1a677a07791f6dc910a345aaa0ee6fd6db1
rijen <rijen@rijen-pc>
2015-11-12 10:01:09 +0600

    xxx

--- User/UserList.pm
+++ User/UserList.pm
@@ -14,23 +14,7 @@ use Kernel::System::VariableCheck qw(IsHashRefWithData);
 use Kernel::System::ObjectManager;
 local $Kernel::OM = Kernel::System::ObjectManager->new();
 our $ObjectManagerDisabled = 1;
-=head1 NAME
 
-Kernel::GenericInterface::Operation::Test - GenericInterface Operation Test backend
-
-=head1 SYNOPSIS
-
-=head1 PUBLIC INTERFACE
-
-=over 4
-
-=cut
-
-=item new()
-
-usually, you want to create an instance of this
-by using Kernel::GenericInterface::Operation->new();
-=cut
 
 sub new {
 	my ( $Type, %Param ) = @_;
@@ -57,66 +41,18 @@ sub new {
 	}
 	return $Self;
 }
-=item Run()
 
-perform the selected test Operation. This will return the data that
-was handed to the function or return a variable data if 'TestError' and
-'ErrorData' params are sent.
-
-    my $Result = $OperationObject->Run(
-Data => {
-	# data payload before Operation
-            ...
-}
-,
-);
-$Result = {
-	Success         => 1,                   # 0 or 1
-        ErrorMessage    => '',                  # in case of error
-	Data            => {
-		# result data payload after Operation
-            ...
-	}
-	,
-};
-my $Result = $OperationObject->Run(
-Data => {
-	# data payload before Operation
-            TestError   => 1,
-	ErrorData   => {
-		...
-	}
-	,
-}
-,
-);
-$Result = {
-	Success         => 0,                                   # it always return 0
-        ErrorMessage    => 'Error message for error code: 1',   # including the 'TestError' param
-	Data            => {
-		ErrorData   => {
-			# same data was sent as
-                                                                # 'ErrorData' param
-		}
-		,
-            ...
-	}
-	,
-};
-=cut
 
 sub Run {
 	my ( $Self, %Param ) = @_;
 	my $UserObject = $Kernel::OM->Get('Kernel::System::User');
 	my %List = $UserObject->UserList();
 	my @Result;
-	for my $key(keys %List) {
+	foreach my $key(keys %List) {
 		my $UserObject = $Kernel::OM->Get('Kernel::System::User');
 		my %UserEntry = $UserObject->GetUserData(UserID=>$key);
-		my $UserBundle = {
-			%UserEntry,
-		};
-		push @Result, $UserBundle;
+		push @Result, \%UserEntry;
+		
 	}
 	# return result
 	return {
@@ -125,14 +61,4 @@ sub Run {
 	};
 }
 1;
-=back
 
-=head1 TERMS AND CONDITIONS
-
-This software is part of the OTRS project (L<http://otrs.org/>).
-
-This software comes with ABSOLUTELY NO WARRANTY. For details, see
-the enclosed file COPYING for license information (AGPL). If you
-did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
-
-=cut
